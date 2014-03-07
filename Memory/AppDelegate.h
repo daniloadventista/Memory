68fd71701b992bf69ae456704a45da6839da957d
danilo <danilo.adventista@gmail.com>
2014-03-07 14:14:07 -0300

    Initial Commit

---
+++ Memory/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Memory
+//
+//  Created by Danilo Lira de Lima on 07/03/14.
+//  Copyright (c) 2014 danilo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Memory/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Memory
+//
+//  Created by Danilo Lira de Lima on 07/03/14.
+//  Copyright (c) 2014 danilo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
